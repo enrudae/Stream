700d5519e684e20cc4fb8ff345d86f512477ab5d
MelixQ <melixmilan@gmail.com>
2023-07-13 20:16:37 +0500

    Bad git push fix

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from music.models import Track
 from user.models import CustomUser, MusicianProfile
 from Stream.yandex_s3_storage import ClientDocsStorage
 
@@ -6,10 +7,12 @@ from Stream.yandex_s3_storage import ClientDocsStorage
 class Post(models.Model):
     text = models.TextField(max_length=255)
     image = models.FileField(storage=ClientDocsStorage(), null=True, blank=True)
+    track = models.FileField(storage=ClientDocsStorage(), null=True, blank=True)
     like_count = models.IntegerField(default=0)
     created_date = models.DateTimeField(auto_now_add=True)
     is_deleted = models.BooleanField(default=False)
     musician = models.ForeignKey(MusicianProfile, verbose_name='Музыкант', on_delete=models.CASCADE)
+    #track = models.ForeignKey(Track, verbose_name='Трек', on_delete=models.CASCADE)
 
 
 class CommentInPost(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,77 @@
+import rest_framework.viewsets
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, SAFE_METHODS
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from blog.models import *
+from blog.permissions import IsOwner, IsOwnerOrReadOnly, IsAuthenticatedAndSafe
+from blog.serializers import PostSerializer, PostCreateSerializer
 
-# Create your views here.
+
+# Get all posts
+class PostsAPIView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+# Get and Delete post by id
+class PostListDestroyAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def check_permissions(self, request):
+        super().check_permissions(request)
+        if request.method in SAFE_METHODS:
+            return True
+        instance = self.get_object()
+        if instance.musician.user != request.user:
+            raise PermissionDenied(detail=("Cannot delete other people posts",))
+
+
+# Get all musician's posts
+class MusicianPostsAPIView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        musician_id = self.kwargs.get('pk', None)
+        return Post.objects.filter(musician=musician_id)
+
+
+# Create new post
+class NewMusicianPostAPIView(generics.CreateAPIView):
+    serializer_class = PostCreateSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Post.objects.all()
+
+    def check_permissions(self, request):
+        super().check_permissions(request)
+        if not request.user.is_musician:
+            raise PermissionDenied(detail=("Only musicians can create posts",))
+
+
+class ModifyMusicianPostAPIView(generics.UpdateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Post.objects.all()
+
+    def check_permissions(self, request):
+        super().check_permissions(request)
+        if not request.user.is_musician:
+            raise PermissionDenied(detail=("Only musicians can modify posts",))
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user != instance.musician.user:
+            return Response({'Error': 'Cannot modify others posts'})
+        serializer = PostCreateSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
